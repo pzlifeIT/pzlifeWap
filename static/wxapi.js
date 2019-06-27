39c9773db584860ccdf3de0470514d501f70a0ca
wujunjie <917436062@qq.com>
2019-06-27 20:06:21 +0800

    修改

--- static/wxapi.js
+++ static/wxapi.js
@@ -8,7 +8,7 @@ export default {
   wxShowMenu: function (host, share_img, share_title, jump_url, callback) {
     // let api = this.host;
     console.log(host)
-    alert(window.location.origin + '/?hid=' + hid + "&pid=" + pid);
+    alert(jump_url);
     Vue.axios.get(host).then(function (res) {
       var getMsg = res.data.signPackage;
       console.log(getMsg.url)
