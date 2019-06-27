0e955f9227a837c7a59a2f12e1e9e65cd366ade2
wujunjie <917436062@qq.com>
2019-06-27 11:03:09 +0800

    修改

--- src/components/bossActive.vue
+++ src/components/bossActive.vue
@@ -397,7 +397,7 @@
           switch (code) {
             case 200:
               that.uid = res.data.data.uid
-              that.loginStatus = false;
+              that.loginStatus = status;
               that.login = true
               that.status = true
               let locaUrl = window.location.href
